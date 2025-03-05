11cf9c5e865d1b00fef26d456aa876d7d9ea9a8e
Aadil Mansoor <aadilmansoor.am@gmail.com>
2025-03-05 15:49:46 +0530

    add coverage report in github actions

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,6 +1,8 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
+import { render } from 'test/utilities';
 
 test('it should render the component', () => {
   render(<Counter />);
@@ -20,9 +22,32 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  render(<Counter initialCount={7} />);
 
-test.todo(
-  'it should reset the count when the "Reset" button is pressed',
-  async () => {},
-);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('7');
+});
+
+test('it should reset the count when the "Reset" button is pressed', async () => {
+  const { user } = render(<Counter initialCount={5} />);
+
+  const currentCount = screen.getByTestId('current-count');
+
+  const resetButton = screen.getByRole('button', { name: 'Reset' });
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+
+  expect(currentCount).toHaveTextContent('5');
+
+  await user.click(resetButton);
+
+  expect(currentCount).toHaveTextContent('0');
+
+  await user.click(incrementButton);
+
+  expect(currentCount).toHaveTextContent('1');
+
+  await user.click(resetButton);
+
+  expect(currentCount).toHaveTextContent('0');
+});
--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,5 +1,29 @@
-import { render, screen } from 'test/utilities';
-import PackingList from '.';
+import { render as _render, screen } from 'test/utilities';
+import { PackingList } from '.';
+import { expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { createStore } from './store';
+import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+
+const render: typeof _render = (Component, options) => {
+  const store = createStore();
+
+  const Wrapper = ({ children }: PropsWithChildren) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  return _render(Component, { ...options, wrapper: Wrapper });
+};
+
+const renderComponent = () => {
+  return {
+    ...render(<PackingList />),
+    itemInput: screen.getByPlaceholderText('New Item'),
+    addButton: screen.getByRole('button', { name: 'Add New Item' }),
+    user: userEvent.setup(),
+  };
+};
 
 it('renders the Packing List application', () => {
   render(<PackingList />);
@@ -10,19 +34,29 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  renderComponent();
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  const { itemInput, addButton } = renderComponent();
+  expect(itemInput).toHaveValue('');
+  expect(addButton).toBeDisabled();
+});
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { itemInput, addButton, user } = renderComponent();
+  await user.type(itemInput, 'hello');
+  expect(itemInput).toHaveValue('hello');
+  expect(addButton).toBeEnabled();
+  await user.click(addButton);
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { itemInput, addButton, user } = renderComponent();
+  await user.type(itemInput, 'hello');
+  await user.click(addButton);
+
+  const item = screen.getByText('hello');
+  const removeButton = screen.getByRole('button', { name: /remove/i });
+});
